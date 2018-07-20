28e72ea85d220ba64797a0935ca9642c51ee90d1
LucasRCunha <lucasrcunha01@gmail.com>
2018-07-20 14:41:25 -0300

    Projeto Criado

---
+++ src/main/java/com/lucascunha/projetoPedidos/ProjetoPedidosBackApplication.java
@@ -0,0 +1,12 @@
+package com.lucascunha.projetoPedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPedidosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPedidosBackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lucascunha/projetoPedidos/ProjetoPedidosBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lucascunha.projetoPedidos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoPedidosBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
