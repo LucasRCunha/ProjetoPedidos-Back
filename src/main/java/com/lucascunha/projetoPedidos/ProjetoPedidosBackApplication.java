976969142a987f5b35338bcef35a0b55c6e74d10
LucasRCunha <lucasrcunha01@gmail.com>
2018-07-20 17:01:30 -0300

    Criando operacao de instanciacao

--- src/main/java/com/lucascunha/projetoPedidos/ProjetoPedidosBackApplication.java
+++ src/main/java/com/lucascunha/projetoPedidos/ProjetoPedidosBackApplication.java
@@ -1,12 +1,30 @@
 package com.lucascunha.projetoPedidos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoPedidosBackApplication {
+import com.lucascunha.projetoPedidos.domain.Categoria;
+import com.lucascunha.projetoPedidos.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoPedidosBackApplication  implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoPedidosBackApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
