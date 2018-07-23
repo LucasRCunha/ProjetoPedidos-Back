ff5d2fce5cac876c93cd97d60fd5100aafcbd88f
LucasRCunha <lucasrcunha01@gmail.com>
2018-07-23 17:20:26 -0300

    GDT2 MINGW64 /d/Projetos/projetoPedidos-Back (master)

--- src/main/java/com/lucascunha/projetoPedidos/services/CategoriaService.java
+++ src/main/java/com/lucascunha/projetoPedidos/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.lucascunha.projetoPedidos.domain.Categoria;
+import com.lucascunha.projetoPedidos.dto.CategoriaDTO;
 import com.lucascunha.projetoPedidos.repositories.CategoriaRepository;
 import com.lucascunha.projetoPedidos.services.exceptions.DataIntegrityException;
 import com.lucascunha.projetoPedidos.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
