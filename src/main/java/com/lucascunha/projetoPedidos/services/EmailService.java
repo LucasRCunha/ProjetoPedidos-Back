115c989902a700a70d4d8ec33640879134d06cc5
LucasRCunha <lucasrcunha01@gmail.com>
2018-07-25 14:30:55 -0300

    Esqueci a senha

--- src/main/java/com/lucascunha/projetoPedidos/services/EmailService.java
+++ src/main/java/com/lucascunha/projetoPedidos/services/EmailService.java
@@ -2,6 +2,7 @@ package com.lucascunha.projetoPedidos.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.lucascunha.projetoPedidos.domain.Cliente;
 import com.lucascunha.projetoPedidos.domain.Pedido;
 
 public interface EmailService {
@@ -9,4 +10,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
