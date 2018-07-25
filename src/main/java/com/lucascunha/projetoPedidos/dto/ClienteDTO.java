d19afd4f1ede3ddcb97ff55376b2f519efaa3546
LucasRCunha <lucasrcunha01@gmail.com>
2018-07-25 09:58:50 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/lucascunha/projetoPedidos/dto/ClienteDTO.java
+++ src/main/java/com/lucascunha/projetoPedidos/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.lucascunha.projetoPedidos.domain.Cliente;
+import com.lucascunha.projetoPedidos.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
